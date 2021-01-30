9c3b593a7661aa5c2fe8a92e5b521fed50d653ca
Ruairi <roo2319@gmail.com>
2021-01-30 20:49:07 +0000

    Added basic selection

--- evolve.py
+++ evolve.py
@@ -1,3 +1,4 @@
+from os import supports_effective_ids
 import pickle
 import random
 import statistics
@@ -8,17 +9,15 @@ from multiprocessing.context import ProcessError
 import ctrnn
 import line_location
 
-runs_per_net = 10
-simulation_seconds = 2
+simulation_seconds = 3
 time_const = line_location.line_location.timestep
-tasks = ()
+tasks = []
 
 def fitness(genome,tasks):
     sender = ctrnn.CTRNN(genome)
     receiver = ctrnn.CTRNN(genome)
     fitnesses = []
 
-    # for run in range(runs_per_net):
     for sp,rp,goal in tasks:
         sim = line_location.line_location(senderPos=sp,receiverPos=rp, goal=goal)
         sender.reset()
@@ -28,9 +27,7 @@ def fitness(genome,tasks):
         fitness = 0.0
         while sim.t < simulation_seconds:
             senderdx   = sim.motor(sender.eulerStep(sim.getState(True),time_const)[0])
-            receiverinputs = sim.getState(False)
-            receiverinputs[0] = goal
-            receiverdx = sim.motor(receiver.eulerStep(receiverinputs,time_const)[0])
+            receiverdx = sim.motor(receiver.eulerStep(sim.getState(False),time_const)[0])
 
             sim.step(senderdx,receiverdx)
 
@@ -41,18 +38,16 @@ def fitness(genome,tasks):
 
             #print("{0} fitness {1}".format(net, fitness))
 
-
-    # The genome's fitness is its worst performance across all runs.
-    # print(fitnesses,rank_reduce(fitnesses)) 
     return rank_reduce(fitnesses)
 
+
+# Calculate a weighted sum of fitnesses, with stronger weighting to lower scores
 def rank_reduce(fitnesses):
-    # print(fitnesses,sum([elem/(i+1) for i,elem in enumerate(sorted(fitnesses,reverse=True))]))
     s_fitnesses = sorted(fitnesses)
     return sum([elem/(i+1) for i,elem in enumerate(s_fitnesses)])
 
+# Create pop_size random genomes
 def initialise(pop_size):
-    
     pop = []
     while len(pop)<pop_size:
         genome = ctrnn.Genome()
@@ -60,18 +55,21 @@ def initialise(pop_size):
 
     return pop
 
+# Calculate fitness score for each member of the population
 def assess(pop, pool):
-    # You could pass in a list of trials here
     global tasks 
-    tasks = [(random.uniform(0,0.3),random.uniform(0,0.3),goal) for goal in [0.55,0.65,0.75,0.85,0.95]]
+    goals = [0.55,0.65,0.75,0.85,0.95]
+    tasks = [(random.uniform(0,0.3),random.uniform(0,0.3),goal) for goal in goals]
     pop = [(x,tasks) for x in pop]
     pop = pool.starmap(assess_item, pop)
     return sorted(pop, key = lambda i: i["fitness"], reverse=True) 
 
+# Calculate the fitness of a single genome
 def assess_item(item,tasks):
     item["fitness"] = fitness(item["genome"],tasks)
     return item
 
+# Mutate each member of the population
 def mutate(pop, pool):
     global tasks
     pop = [(x,tasks) for x in pop]
@@ -79,6 +77,7 @@ def mutate(pop, pool):
             
     return pop
 
+# Mutate a single genome, only keep the result if it's better than the parent
 def mutate_item(item,tasks):
     child = item["genome"].copy()
     child.mutate()
@@ -88,35 +87,36 @@ def mutate_item(item,tasks):
     return item
 
 
-def expand(pop):
+# Replace 0 fitness members of the population with strong fitness members
+def select(pop):
     size = len(pop)
-    for idx, i in enumerate(pop):
-        if i["fitness"] == 0:
-            pop = pop[:idx]
-            break
-    while len(pop) < size:
-        pop.extend(pop.copy())
+    survivors = (9*size)//10
+    pop = pop[:survivors]
+    for i in range(size-survivors):
+        pop.append(pop[i].copy())
     return sorted(pop[:size], key = lambda i: i["fitness"], reverse=True) 
+
 
 def evolve(pop_size=100, max_gen=1, write_every=1, file=None):
 
     random.seed()
 
-    mutation = 0.01
-
     with Pool(processes=cpu_count()) as pool:
 
         pop = initialise(pop_size)
         pop = assess(pop, pool)
-        # This might be cursed???
-        # Can be replaced with selection
-        # pop = expand(pop)
         generation = 0
         best = pop[0]
+        batch_start = time.time()
         while generation < max_gen:
             if file != None and generation % 20 == 0:
                 print(f"Generation {generation}")
+                print(f"Batch Time {time.time()-batch_start}")
+                print(f"Mean fitness {statistics.mean([x['fitness'] for x in pop])}")
+                batch_start = time.time()
+
             generation += 1
+            pop = select(pop)
             pop = mutate(pop, pool)
             pop = assess(pop, pool)
             best = pop[0]
@@ -134,25 +134,6 @@ def write_fitness(pop, gen, file=None):
 
     line = "{:4d}: max:{:.3f}, min:{:.3f}, mean:{:.3f}".format(gen,max(fitness),min(fitness),statistics.mean(fitness))
 
-    # genome = pop[0]["genome"]
-    # sender = ctrnn.CTRNN(genome)
-    # receiver = ctrnn.CTRNN(genome)
-    # sim = line_location.line_location(senderPos=0.3,receiverPos=0.1,goal=0.75)
-    # sender.reset()
-    # receiver.reset()
-    # # Run the given simulation for up to num_steps time steps.
-    # fitness = 0.0
-    # while sim.t < simulation_seconds:
-    #     senderdx = sim.motor(sender.eulerStep(sim.getState(True),time_const)[0])
-    #     receiverdx = sim.motor(receiver.eulerStep(sim.getState(False),time_const)[0])
-    #     sim.step(senderdx,receiverdx)
-    # fitness = sim.fitness()
-    # print("Final conditions:")
-    # print("   Sender = {0:.4f}".format(sim.senderPos))
-    # print(" Receiver = {0:.4f}".format(sim.receiverPos))
-    # print("     Goal = {0:.4f}".format(sim.goal))
-    # print("  Fitness = {0:.4f}".format(sim.fitness()))
-
     if file:
         file.write(line+"\n")
     else:
@@ -160,8 +141,10 @@ def write_fitness(pop, gen, file=None):
 
 if __name__ == '__main__':
     start = time.time()
-    with open("poglog.txt",'w') as f:
-        best = evolve(128,1000,file=f)
+    path = f"logs/{int(start)}.txt"
+    with open(path,'w') as f:
+        print(f"Logs are in {path}")
+        best = evolve(128,200,file=f)
         print(best["fitness"])
         with open("best_genome.pkl",'wb') as g:
             pickle.dump(best["genome"],g)
