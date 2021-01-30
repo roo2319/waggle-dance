99f29fd3e12be46adcef827a427ab1dc6428d14f
Ruairi <roo2319@gmail.com>
2021-01-30 20:49:57 +0000

    World size is now fixed

--- line_location.py
+++ line_location.py
@@ -4,20 +4,19 @@ import random
 class line_location():
     timestep = 0.01
 
-    def __init__(self,worldSize=1,senderPos=None,receiverPos=None,goal=None):
-        self.worldSize = worldSize
+    def __init__(self,senderPos=None,receiverPos=None,goal=None):
         if goal == None:
-            goal = random.uniform(0.5*worldSize,worldSize)
+            goal = random.uniform(0.5,1)
         if senderPos == None:
-            senderPos = random.uniform(0,0.3*worldSize)
+            senderPos = random.uniform(0,0.3)
         if receiverPos == None:
-            receiverPos = random.uniform(0,0.3*worldSize)
+            receiverPos = random.uniform(0,0.3)
         self.goal = goal
         self.senderPos = senderPos
         self.receiverPos = receiverPos
         self.t = 0
 
-    # Currently a simple version with no acceleration
+    # Step, currently has no momentum
     def step(self, senderdx=0, receieverdx=0):
         self.t += self.timestep
         
@@ -28,12 +27,6 @@ class line_location():
     def updatePos(self,isSender,dx):
         pos = self.senderPos if isSender else self.receiverPos
         pos += dx
-
-        # There actually isn't a restriction on bounds, wow!
-        # if pos < 0:
-        #     pos = 0
-        # elif pos > self.worldSize:
-        #     pos = self.worldSize
         
         if isSender:
             # This is where additional checks can be performed on the senders movement
@@ -44,7 +37,7 @@ class line_location():
 
     # Return the sensors, that is the Position, Contact and Target Position sensors
     def getState(self,isSender):
-        contactSensor = 1 if abs(self.senderPos - self.receiverPos) <= 0.04 else 0
+        contactSensor = 1 if abs(self.senderPos - self.receiverPos) <= 0.2 else 0
         if isSender:
             targetSensor = abs(self.senderPos - self.goal)
             return [contactSensor,self.senderPos,targetSensor]
@@ -54,25 +47,27 @@ class line_location():
     
     def getAsciiState(self):
         state = ['-'] * 101 #Positions from 0 to 100
-        if 0 < self.senderPos < self.worldSize:
-            state[floor((self.senderPos/self.worldSize)*100)] = 'S'
-        if 0 < self.receiverPos < self.worldSize:
-            state[floor((self.receiverPos/self.worldSize)*100)] = 'R'
-        state[floor((self.goal/self.worldSize)*100)] = '#'
+        if 0 < self.senderPos < 1:
+            state[floor(self.senderPos*100)] = 'S'
+        if 0 < self.receiverPos < 1:
+            state[floor(self.receiverPos*100)] = 'R'
+        state[floor(self.goal*100)] = '#'
         return f"t = {self.t:.2f}\n" + ''.join(state)
 
     # linear scale, high when receiver is close to goal
     def fitness(self):
         # print(f"Receiver at {self.receiverPos}, fitness {max(self.worldSize - abs(self.receiverPos-self.goal),0)}")
-        return max(self.worldSize - abs(self.receiverPos-self.goal),0)
+        # Receiver goal
+        return max(1 - abs(self.receiverPos-self.goal),0)
+        # Sender Goal
+        # return max(1 - abs(self.senderPos-self.goal),0)
 
-    # motor must always go
-    # make this contnuous
+    # Threshold function
     def motor(self,val):
         if val < 0.25:
-            return -self.worldSize/100
+            return -0.01
         elif val > 0.75: 
-            return self.worldSize/100
+            return 0.01
         else:
             return 0
 
@@ -83,7 +78,6 @@ def main():
         moves = random.choices([-1,-2,0,1,2],k = 2)
         sim.step(*moves)
         print(sim.getAsciiState())
-        # time.sleep(0.01)
     print(f"Fitness is {sim.fitness()}\nSender has sensors {sim.getState(True)}\nReceiver has sensors {sim.getState(False)}")
 
 if __name__ == '__main__':
