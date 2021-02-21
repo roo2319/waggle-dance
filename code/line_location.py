d07405daed814bebb912aaa2730401aea443c5b3
Ruairi <roo2319@gmail.com>
2021-02-21 21:03:54 +0000

    changed clippedmotor3

--- code/line_location.py
+++ code/line_location.py
@@ -42,7 +42,7 @@ def clippedMotor2(val):
     return quickClip(-0.01,0.01,(val-1)/50)
 
 def clippedMotor3(val):
-    return quickClip(-0.01,0.01,val)
+    return quickClip(-0.01,0.01,val-0.5)
 
 def sigmoidMotor(val):
     return (expit(val)-0.5)/50 
